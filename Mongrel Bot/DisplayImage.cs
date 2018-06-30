ea1eb4558c522ad17a8f718dd2f45fffaada89cb
grong <lionel.de-sousa@epitech.eu>
2018-06-30 16:17:04 +0200

    add tags

--- Mongrel Bot/DisplayImage.cs
+++ Mongrel Bot/DisplayImage.cs
@@ -7,25 +7,33 @@ namespace Mongrel_Bot
 {
     public class DisplayImage : ModuleBase
     {
-        [Command("image", RunMode = RunMode.Async), Summary("Display an image")]
-
-        public async Task GetImage()
+        private string Addrand(params string[] ag)
         {
             using (WebClient wc = new WebClient())
             {
-                string pj = wc.DownloadString("http://safebooru.org/index.php?page=dapi&s=post&q=index&limit=1&pid=");
+                string pj = wc.DownloadString("http://safebooru.org/index.php?page=dapi&s=post&q=index&limit=1&tags=" + String.Join("+", ag));
                 string[] post = pj.Split('"');
-
                 int max = Convert.ToInt32(post[5]);
                 int rand = Program.rand.Next(max);
-                string jp = wc.DownloadString("http://safebooru.org/index.php?page=dapi&s=post&q=index&limit=1&pid=" + rand);
-                string[] str = jp.Split(new string[] { "file_url" }, StringSplitOptions.RemoveEmptyEntries);
+                string jp = wc.DownloadString("http://safebooru.org/index.php?page=dapi&s=post&q=index&limit=1&pid=" + rand + "&tags=" + String.Join("+", ag));
+                return (jp);
+            }
+        }
+
+        [Command("RandImage", RunMode = RunMode.Async), Summary("Display an image")]
+        public async Task GetImage(params string[] ag)
+        {
+            try
+            {
+                string FinalString = Addrand(ag);
+                string[] str = FinalString.Split(new string[] { "file_url" }, StringSplitOptions.RemoveEmptyEntries);
                 string[] str2 = str[1].Split(new char[] { '"' }, StringSplitOptions.RemoveEmptyEntries);
-                string str3 = "http:" + str2[1];
+                string str3 = "https:" + str2[1];
 
                 await ReplyAsync("Here goes nothing");
                 await ReplyAsync(str3);
-            }
+              
+            }catch(Exception E) { await ReplyAsync(E.Message); }
         }
     }
 }
