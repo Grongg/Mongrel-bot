53cd0c72c24f8df7cb8b900df9ac8a020edc6814
grong <lionel.de-sousa@epitech.eu>
2018-07-02 00:15:34 +0200

    pimp the help message

--- Mongrel Bot/help.cs
+++ Mongrel Bot/help.cs
@@ -10,7 +10,7 @@ namespace Mongrel_Bot
         public async Task GetHelp()
         {
                 await ReplyAsync("Here comes the help :\n\n");
-                await ReplyAsync("To have random image u can type:\nRandImage gelbooru/safebooru with/without any command for more precision\n\nOf course u can use the tag help to see this message again");
+                await ReplyAsync("To have random image u can type:\nRandImage ```fix\n gelbooru/safebooru``` with tags for more precision\n\nFor exemple:\n```fix\n@mongrelBot RandImage\n@mongrelBot Randimage safebooru\n@mongrelBot Randimage gelbooru(nsfw only)\n```Of course u can use the tag help to see this message again");
         }
     }
 }
